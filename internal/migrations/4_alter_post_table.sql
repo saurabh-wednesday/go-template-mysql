b72e1c6870a8c4dbd1f7864af00a938048b87795
saurabh-wednesday <saurabh@wednesday.is>
2023-02-16 16:27:26 +0530

    fix(authors): add label to foreign key

--- internal/migrations/4_alter_post_table.sql
+++ internal/migrations/4_alter_post_table.sql
@@ -2,8 +2,8 @@
 -- +migrate Up
 
 ALTER TABLE post DROP FOREIGN KEY post_ibfk_1;
-ALTER TABLE post ADD FOREIGN KEY(author_id) REFERENCES authors(id);
+ALTER TABLE post ADD CONSTRAINT author_id_fk FOREIGN KEY(author_id) REFERENCES authors(id);
 -- +migrate Down
 
-ALTER TABLE post DROP FOREIGN KEY post_ibfk_1;
-ALTER TABLE post ADD FOREIGN KEY (author_id) REFERENCES users(id);
+ALTER TABLE post DROP FOREIGN KEY author_id_fk;
+ALTER TABLE post ADD CONSTRAINT post_ibfk_1 FOREIGN KEY (author_id) REFERENCES users(id);
