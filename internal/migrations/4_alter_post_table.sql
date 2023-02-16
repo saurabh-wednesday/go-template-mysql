2a90835c5bbf683375f9aaf1b53d944c652ee474
saurabh-wednesday <saurabh@wednesday.is>
2023-02-16 16:14:23 +0530

    fix(authors): down migration query

--- internal/migrations/4_alter_post_table.sql
+++ internal/migrations/4_alter_post_table.sql
@@ -6,4 +6,4 @@ ALTER TABLE post ADD FOREIGN KEY(author_id) REFERENCES authors(id);
 -- +migrate Down
 
 ALTER TABLE post DROP FOREIGN KEY post_ibfk_1;
-ALTER TABLE post FOREIGN KEY (author_id) REFERENCES users(id);
+ALTER TABLE post ADD FOREIGN KEY (author_id) REFERENCES users(id);
